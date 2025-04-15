af03b65248914e436419d6a7e403b4c952085336
TatianaNoskova <tatiana.noskova@davinci.edu.ar>
2025-04-14 21:20:07 -0300

    commit_2_140425

--- torneo_futbal/src/torneo_futbal/Club.java
+++ torneo_futbal/src/torneo_futbal/Club.java
@@ -1,5 +1,21 @@
 package torneo_futbal;
 
 public class Club {
+	
+	private String nombre;
+	
+	public Club(String nombre) {
+        this.nombre = nombre;
+      
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    @Override
+    public String toString() {
+        return nombre ;
+    }
 
 }
